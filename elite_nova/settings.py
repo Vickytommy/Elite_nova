802628e312df9c93156abc4877b7389e84332f68
Victor Odesola <vickytommy785@gmail.com>
2025-01-13 12:27:02 +0100

    Update settings.py

--- elite_nova/settings.py
+++ elite_nova/settings.py
@@ -133,7 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'he'
+
 
 TIME_ZONE = 'Asia/Jerusalem'
 
